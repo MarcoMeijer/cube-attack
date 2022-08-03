89fb5142dd413c66a5b01593570b5ffbfe54d2b4
Marco Meijer <marcomeijerm@gmail.com>
2022-08-03 19:43:26 +0200

    homework 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import React, { useRef } from 'react';
+import { Environment, OrbitControls } from '@react-three/drei';
+import { Controls, useControl } from 'react-three-gui';
+import { useFrame } from '@react-three/fiber';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SpinningCube = () => {
+    const box = useRef();
+
+    const roughness = useControl('Roughness', { type: 'number', min: 0, max: 1 });
+    const metalness = useControl('Metalness', { type: 'number', min: 0, max: 1 });
+    const speed = useControl('Speed', { type: 'number', min: 0, max: 10 });
+
+    useFrame(({ clock }) => {
+        box.current.rotation.x += speed * 0.01;
+        box.current.rotation.y += speed * 0.01;
+    });
+
+    return <mesh ref={box}>
+        <boxGeometry />
+        <meshStandardMaterial color={'#ffffff'} roughness={roughness} metalness={metalness} />
+    </mesh>;
+}
+
+const App = () => {
+    return (
+        <Controls.Provider>
+            <Controls.Canvas>
+                <SpinningCube />
+                <OrbitControls />
+                <Environment background={true} files="/neon_photostudio_2k.hdr" />
+            </Controls.Canvas>
+            <Controls />
+        </Controls.Provider>
+    );
 }
 
 export default App;
