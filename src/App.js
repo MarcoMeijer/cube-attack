d9048efda260dc5e2d95cd949e22c2c23b330b0b
Marco Meijer <marcomeijerm@gmail.com>
2022-08-18 17:22:02 +0200

    updating state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './styles/index.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { OrbitControls, Stars } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { EnemyPool } from './components/Enemy';
@@ -9,25 +9,46 @@ import { Effects } from './components/Effects';
 import { Floor } from './components/Floor';
 import { Wave } from './components/Wave';
 import { Hud } from './components/html/Hud';
+import { initialState, StoreContext } from './hooks/useStore';
 
 const App = () => {
+    const [state, setState] = useState({...initialState});
+    const [dummy, setDummy] = useState(0);
+
+    const setStore = (f) => {
+        const props = f(state);
+        let changed = false;
+        for (const key in props) {
+            if (state[key] !== props[key]) {
+                state[key] = props[key];
+                changed = true;
+            }
+        }
+        if (changed) {
+            setDummy(dummy + 1);
+            setState(state);
+        }
+    }
+    state.setStore = setStore;
+
     return (
-        <>
+        <StoreContext.Provider value={{current: state}}>
             <Canvas mode="concurrent" camera={{ position: [8.5, 10, 20]}}>
-                <Floor />
-                <fog color="#b8bfbe" attach="fog" near={100} far={200} />
-                <TowerPool />
-                <EnemyPool />
-                <Wave />
-                <ProjectilePool />
-                <OrbitControls enablePan={false} target-x={8.5} target-z={8.5}/>
-                <Stars radius={100} depth={50} count={5000} factor={8} saturation={1} fade speed={2} />
-                <Effects />
-                <ambientLight intensity={1}/>
-                {/* <Environment files="/je_gray_park_2k.hdr" /> */}
+                <StoreContext.Provider value={{current: state}}>
+                    <Floor />
+                    <fog color="#b8bfbe" attach="fog" near={100} far={200} />
+                    <TowerPool />
+                    <EnemyPool />
+                    <Wave />
+                    <ProjectilePool />
+                    <OrbitControls enablePan={false} target-x={8.5} target-z={8.5}/>
+                    <Stars radius={100} depth={50} count={5000} factor={8} saturation={1} fade speed={2} />
+                    <Effects />
+                    <ambientLight intensity={1}/>
+                </StoreContext.Provider>
             </Canvas>
             <Hud />
-        </>
+        </StoreContext.Provider>
     );
 }
 
--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,42 +1,32 @@
+import { createContext, useContext } from "react";
 import { Vector3 } from "three";
 
-const createStore = () => {
-    const state = {
-        enemies: [],
-        towers: [],
-        projectiles: [],
-        wave: 0,
-        path: [
-            new Vector3(   8, 0,-0.5),
-            new Vector3(   8, 0,   2),
-            new Vector3(  12, 0,   2),
-            new Vector3(  12, 0,  14),
-            new Vector3(   9, 0,  14),
-            new Vector3(   9, 0,  14),
-            new Vector3(   9, 0,  11),
-            new Vector3(  15, 0,  11),
-            new Vector3(  15, 0,   6),
-            new Vector3(   6, 0,   6),
-            new Vector3(   6, 0,  13),
-            new Vector3(   3, 0,  13),
-            new Vector3(   3, 0,   3),
-            new Vector3(   1, 0,   3),
-            new Vector3(   1, 0,   8),
-            new Vector3(-0.5, 0,   8),
-        ]
-    };
-
-    const setStore = (props) => {
-        for (const key in props) {
-            state[key] = props[key];
-        }
-    }
-
-    state.setStore = setStore;
-
-    return () => {
-        return state;
-    };
+export const initialState = {
+    enemies: [],
+    towers: [],
+    projectiles: [],
+    wave: 0,
+    path: [
+        new Vector3(   8, 0,-0.5),
+        new Vector3(   8, 0,   2),
+        new Vector3(  12, 0,   2),
+        new Vector3(  12, 0,  14),
+        new Vector3(   9, 0,  14),
+        new Vector3(   9, 0,  14),
+        new Vector3(   9, 0,  11),
+        new Vector3(  15, 0,  11),
+        new Vector3(  15, 0,   6),
+        new Vector3(   6, 0,   6),
+        new Vector3(   6, 0,  13),
+        new Vector3(   3, 0,  13),
+        new Vector3(   3, 0,   3),
+        new Vector3(   1, 0,   3),
+        new Vector3(   1, 0,   8),
+        new Vector3(-0.5, 0,   8),
+    ]
 };
 
-export const useStore = createStore();
+export const StoreContext = createContext(initialState);
+
+export const useStore = () => useContext(StoreContext).current;
+
